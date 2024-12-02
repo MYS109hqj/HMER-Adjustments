63676345f53d9f79f19afda5fcae9f693e0630f6
MYS109hqj <qj.st@qq.com>
2024-12-02 14:40:57 +0800

    241202commit

---
+++ CAN/Annotated version/CAN-main_ad/models/densenet.py
@@ -0,0 +1,135 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from thop import profile
+
+
+# DenseNet-B
+class Bottleneck(nn.Module):
+    # Bottleneck 层是 DenseNet 的一个重要部分，包含两个卷积层。
+    # 第一个是 1x1 卷积，减少通道数；第二个是 3x3 卷积，用于特征学习。
+    # 通过 1x1 卷积，减少通道数后，**计算量较小，同时能提取更多有用的特征**。
+    # bn（Batch Normalization批量归一化）的作用是**标准化卷积层输出的特征图**，
+    # 使得每个特征图的均值接近0，标准差接近1，**进一步稳定训练过程**。
+    # Dropout：如果启用 Dropout，则会在每一层后应用 Dropout，防止过拟合。
+    def __init__(self, nChannels, growthRate, use_dropout):
+        super(Bottleneck, self).__init__()
+        interChannels = 4 * growthRate
+        self.bn1 = nn.BatchNorm2d(interChannels)
+        self.conv1 = nn.Conv2d(nChannels, interChannels, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(growthRate)
+        self.conv2 = nn.Conv2d(interChannels, growthRate, kernel_size=3, padding=1, bias=False)
+        self.use_dropout = use_dropout
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        if self.use_dropout:
+            out = self.dropout(out)
+        out = F.relu(self.bn2(self.conv2(out)), inplace=True)
+        if self.use_dropout:
+            out = self.dropout(out)
+        out = torch.cat((x, out), 1)
+        return out
+
+
+# single layer
+class SingleLayer(nn.Module):
+    # SingleLayer 层只包含一个 3x3 的卷积层。它没有像 Bottleneck 层那样使用 1x1 卷积减少通道数，
+    # 因此计算量稍大一些，但结构更简单。
+    def __init__(self, nChannels, growthRate, use_dropout):
+        super(SingleLayer, self).__init__()
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.conv1 = nn.Conv2d(nChannels, growthRate, kernel_size=3, padding=1, bias=False)
+        self.use_dropout = use_dropout
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(x, inplace=True))
+        if self.use_dropout:
+            out = self.dropout(out)
+        out = torch.cat((x, out), 1)
+        return out
+
+
+# transition layer
+class Transition(nn.Module):
+    # Transition 层的作用是连接 DenseBlock 和 DenseBlock 之间，并减少通道数。
+    # 它使用 1x1 卷积来压缩通道数，然后应用平均池化（avg_pool2d）来下采样空间尺寸。
+    def __init__(self, nChannels, nOutChannels, use_dropout):
+        super(Transition, self).__init__()
+        self.bn1 = nn.BatchNorm2d(nOutChannels)
+        self.conv1 = nn.Conv2d(nChannels, nOutChannels, kernel_size=1, bias=False)
+        self.use_dropout = use_dropout
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        if self.use_dropout:
+            out = self.dropout(out)
+        out = F.avg_pool2d(out, 2, ceil_mode=True)
+        return out
+
+
+class DenseNet(nn.Module):
+    def __init__(self, params):
+        super(DenseNet, self).__init__()
+        growthRate = params['densenet']['growthRate'] # 定义每个 DenseBlock 中每层的通道增加量
+        reduction = params['densenet']['reduction'] # 控制在 Transition 层中减少通道数的比例
+        bottleneck = params['densenet']['bottleneck'] # 是否使用 Bottleneck 层（即使用 1x1 卷积来减少通道数）
+        use_dropout = params['densenet']['use_dropout'] # 是否在模型中使用 Dropout
+
+        # DenseBlock 是 DenseNet 模型中的基本模块，每个 DenseBlock 都由多个卷积层组成。
+        # 在每个 DenseBlock 内，层与层之间是密切连接的，每一层都会将其输出与前面的所有层的输出进行拼接。
+        # nDenseBlocks = 16 意味着在模型中总共有 16 个这样的 DenseBlock，每个 DenseBlock 包含多个卷积层。
+        # 每个 DenseBlock 都会增加通道数（根据增长率 growthRate : growthRate 是 DenseNet 中每个 DenseBlock 层增加的通道数）。
+        nDenseBlocks = 16
+        # nChannels 表示当前层的输出通道数
+        # 2 * growthRate 是模型的初始通道数，通常在 DenseNet 中，初始通道数是 2 * growthRate（可以理解为网络一开始的特征图宽度）
+        nChannels = 2 * growthRate
+        self.conv1 = nn.Conv2d(params['encoder']['input_channel'], nChannels, kernel_size=7, padding=3, stride=2, bias=False)
+        self.dense1 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck, use_dropout)
+        # 这行代码更新 nChannels，它表示 DenseBlock 完成后，输出通道数的变化。
+        # 因为每个 DenseBlock 中有 nDenseBlocks 层，每层都会增加 growthRate 个通道，因此输出的通道数应该增加 nDenseBlocks * growthRate。
+        nChannels += nDenseBlocks * growthRate
+        # nChannels 是 DenseBlock 完成后的输出通道数，而 reduction 是一个控制比例的因子，用于减少通道数。
+        # reduction 通常小于 1（比如 0.5），意味着我们希望将通道数减小到原来的 reduction 倍。
+        # math.floor 函数用于取整，使得最终的通道数是一个整数。
+        nOutChannels = int(math.floor(nChannels * reduction))
+        # 这行代码创建了一个 Transition 层，它将 nChannels 转换为 nOutChannels，并进行下采样。
+        # Transition 层通过 1x1 卷积减少通道数，并通过池化操作减小空间尺寸。
+        self.trans1 = Transition(nChannels, nOutChannels, use_dropout)
+
+        nChannels = nOutChannels
+        self.dense2 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck, use_dropout)
+        nChannels += nDenseBlocks * growthRate
+        nOutChannels = int(math.floor(nChannels * reduction))
+        self.trans2 = Transition(nChannels, nOutChannels, use_dropout)
+
+        nChannels = nOutChannels
+        self.dense3 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck, use_dropout)
+
+    def _make_dense(self, nChannels, growthRate, nDenseBlocks, bottleneck, use_dropout):
+        # 用于构建一个DenseBlock
+        layers = []
+        # 根据传入的参数，创建多个 Bottleneck 或 SingleLayer 层，并将它们连接成一个 Sequential 模块。
+        # 这个模块会在模型的前向传播过程中作为一个整体使用。
+        for i in range(int(nDenseBlocks)):
+            if bottleneck:
+                layers.append(Bottleneck(nChannels, growthRate, use_dropout))
+            else:
+                layers.append(SingleLayer(nChannels, growthRate, use_dropout))
+            nChannels += growthRate
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = F.relu(out, inplace=True)
+        out = F.max_pool2d(out, 2, ceil_mode=True)
+        out = self.dense1(out)
+        out = self.trans1(out)
+        out = self.dense2(out)
+        out = self.trans2(out)
+        out = self.dense3(out)
+        return out
---
+++ CAN/Annotated version/CAN-main_ad/training.py
@@ -0,0 +1,102 @@
+import torch
+from tqdm import tqdm
+from utils import update_lr, Meter, cal_score
+
+
+def train(params, model, optimizer, epoch, train_loader, writer=None):
+    model.train()
+    device = params['device']
+    loss_meter = Meter()  # 记录损失
+    word_right, exp_right, length, cal_num = 0, 0, 0, 0
+
+    with tqdm(train_loader, total=len(train_loader)//params['train_parts']) as pbar:
+        for batch_idx, (images, image_masks, labels, label_masks) in enumerate(pbar):
+            images, image_masks, labels, label_masks = images.to(device), image_masks.to(
+                device), labels.to(device), label_masks.to(device)
+            batch, time = labels.shape[:2]  # labels.shape[:2] 会返回 (batch_size, max_length)
+            # batch_size：当前 batch 的样本数量。 max_length：当前 batch 中最长的标签长度，包括结束符 (eos)。
+            # 动态更新学习率，默认采用余弦退火策略(cosine)
+            if not 'lr_decay' in params or params['lr_decay'] == 'cosine':
+                update_lr(optimizer, epoch, batch_idx, len(train_loader), params['epochs'], params['lr'])
+            optimizer.zero_grad()
+            # 进行前向传播并计算损失
+            probs, counting_preds, word_loss, counting_loss = model(images, image_masks, labels, label_masks)
+            loss = word_loss + counting_loss
+            loss.backward()
+
+            # clip_grad_norm_ 函数会将所有参数的梯度向量（model.parameters()）的范数 (norm) 限制在一个给定的阈值 (params['gradient']) 内，防止梯度爆炸
+            if params['gradient_clip']:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), params['gradient'])
+            optimizer.step() # 使用优化器更新模型权重
+            loss_meter.add(loss.item())
+
+            # wordRate：评估模型在符号或单词级别的准确性，更细粒度地反映错误率。
+            # ExpRate：评估整个表达式的整体准确性，是一个更严格的指标。
+            wordRate, ExpRate = cal_score(probs, labels, label_masks)
+            word_right = word_right + wordRate * time
+            exp_right = exp_right + ExpRate * batch
+            length = length + time
+            cal_num = cal_num + batch
+
+            if writer:
+                current_step = epoch * len(train_loader) // params['train_parts'] + batch_idx + 1
+                writer.add_scalar('train/word_loss', word_loss.item(), current_step)
+                writer.add_scalar('train/counting_loss', counting_loss.item(), current_step)
+                writer.add_scalar('train/loss', loss.item(), current_step)
+                writer.add_scalar('train/WordRate', wordRate, current_step)
+                writer.add_scalar('train/ExpRate', ExpRate, current_step)
+                writer.add_scalar('train/lr', optimizer.param_groups[0]['lr'], current_step)
+
+            pbar.set_description(f'{epoch+1} word_loss:{word_loss.item():.4f} counting_loss:{counting_loss.item():.4f} WRate:{word_right / length:.4f} '
+                                 f'ERate:{exp_right / cal_num:.4f}')
+            if batch_idx >= len(train_loader) // params['train_parts']:
+                break
+
+        if writer:
+            writer.add_scalar('epoch/train_loss', loss_meter.mean, epoch+1)
+            writer.add_scalar('epoch/train_WordRate', word_right / length, epoch+1)
+            writer.add_scalar('epoch/train_ExpRate', exp_right / cal_num, epoch + 1)
+        return loss_meter.mean, word_right / length, exp_right / cal_num
+
+
+def eval(params, model, epoch, eval_loader, writer=None):
+    model.eval()
+    device = params['device']
+    loss_meter = Meter()
+    word_right, exp_right, length, cal_num = 0, 0, 0, 0
+
+    with tqdm(eval_loader, total=len(eval_loader)//params['valid_parts']) as pbar, torch.no_grad():
+        for batch_idx, (images, image_masks, labels, label_masks) in enumerate(pbar):
+            images, image_masks, labels, label_masks = images.to(device), image_masks.to(
+                device), labels.to(device), label_masks.to(device)
+            batch, time = labels.shape[:2]
+            # probs 是模型输出的每个字符位置的概率分布
+            probs, counting_preds, word_loss, counting_loss = model(images, image_masks, labels, label_masks, is_train=False)
+            loss = word_loss + counting_loss
+            loss_meter.add(loss.item())
+
+            wordRate, ExpRate = cal_score(probs, labels, label_masks)
+            word_right = word_right + wordRate * time
+            exp_right = exp_right + ExpRate * batch # ExpRate：单个batch中公式识别正确率
+            length = length + time
+            cal_num = cal_num + batch
+
+            if writer:
+                current_step = epoch * len(eval_loader)//params['valid_parts'] + batch_idx + 1
+                writer.add_scalar('eval/word_loss', word_loss.item(), current_step)
+                writer.add_scalar('eval/counting_loss', counting_loss.item(), current_step)
+                writer.add_scalar('eval/loss', loss.item(), current_step)
+                writer.add_scalar('eval/WordRate', wordRate, current_step)
+                writer.add_scalar('eval/ExpRate', ExpRate, current_step)
+
+            pbar.set_description(f'{epoch+1} word_loss:{word_loss.item():.4f} counting_loss:{counting_loss.item():.4f} WRate:{word_right / length:.4f} '
+                                 f'ERate:{exp_right / cal_num:.4f}')
+            if batch_idx >= len(eval_loader) // params['valid_parts']:
+                break
+
+        if writer:
+            writer.add_scalar('epoch/eval_loss', loss_meter.mean, epoch + 1)
+            writer.add_scalar('epoch/eval_WordRate', word_right / length, epoch + 1)
+            writer.add_scalar('epoch/eval_ExpRate', exp_right / len(eval_loader.dataset), epoch + 1)
+        return loss_meter.mean, word_right / length, exp_right / cal_num
+        
